6fb632affaf46dabda223958868dea84a089a2e7
Sanyam Sharma <sanyamsharma170@gmail.com>
2026-01-04 19:09:51 +0530

    Set up auth and Intialize header

--- app/layout.js
+++ app/layout.js
@@ -14,13 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${inter.className}`}>
+      <body className={`${inter.className}`}>
         {/* header */}
-        <Header/>
-        <main className="min-h-screen">
-        {children}
-        </main>
+        <Header />
+        <main className="min-h-screen">{children}</main>
         {/* footer */}
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
--- components/header.jsx
+++ components/header.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import { SignedOut, SignedIn, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { LayoutDashboard, PenBox } from 'lucide-react';
 const Header = () => {
   return (
-    <div>
-        <SignedOut>
-            <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
-    </div>
-  )
-}
+    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+      <nav className='conatiner mx-auto px-4 py-4 flex items-center justify-between'>
+        <Link href="/">
+        <Image src="/logo.png" alt="finman logo" height={60} width={200} className='h-15 w-auto object-contain'/>
+        </Link>
 
-export default Header
+        <div className='flex items-center space-x-4'>
+        <SignedIn>
+          <Link href={"/dashboard"} >
+            <Button variant='outline' className='text-grey-600 hover:text-blue-600 flex items-center gap-2'>
+              <LayoutDashboard size={18} /> 
+              <span className='hidden md:inline'>Dashboard</span>
+            </Button>
+          </Link>
+
+          <Link href={"/transaction/create"}>
+            <Button  className='flex items-center gap-2'>
+              <PenBox size={18} /> 
+              <span className='hidden md:inline'>Add Transaction</span>
+            </Button>
+          </Link>
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton forceRedirectUrl='/dashboard'>
+          <Button variant='outline'>Login</Button>
+          </SignInButton>  
+        </SignedOut>
+
+        <SignedIn>
+            <UserButton appearance={{
+              elements: {
+                avatarBox: "w-10 h-10",
+              },
+             }}
+            />
+        </SignedIn>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
